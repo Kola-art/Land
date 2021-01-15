02a65028ea3e1f12fd3d81ecd1f3b25480bc2e48
Artem Kolodka <73796530+Kola-art@users.noreply.github.com>
2021-01-16 01:02:28 +0200

    Added gulpfile.js

--- README.md
+++ README.md
@@ -8,4 +8,8 @@
 * Создаем .gitignore, проделываем коммит, пушим)
 >git 
 * Cоздаем package.json 
-> npm install 
+> npm install (при существ package.json устанавливает пакеты которые указаны в файле)
+* Добавляем пакеты
+>npm install browser-sync gulp gulp-pug gulp-sass  --save-dev   
+
+* Создаем gulpfile.js, подключаем Gulp локально, подключение в gulpfile.js (https://browsersync.io/docs/gulp, https://www.npmjs.com/package/  -- создаем const, подключаем таски для галпа )
---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const spritesmith = require('gulp.spritesmith');
+const rimraf = require('rimraf');
+const rename = require('gulp-rename');
+
+//  ----------- SERVER -----------
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            port:9000,
+            baseDir: "build"
+        }
+    });
+
+
+    gulp.watch('build/**/*').on('change', browserSync.reload);
+});
+
+// ------- Pug compile ------------
+gulp.task('templates:compile', function buildHTML() {
+    return gulp.src('source/template/index.pug')
+    .pipe(pug({
+       pretty: true
+    }))
+    .pipe(gulp.dest('build'));
+  });
+
+
+//   ------ styles compile -------
+gulp.task('styles:compile', function () {
+    return gulp.src('source/styles/main.scss')
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(rename ('main.min.css'))
+      .pipe(gulp.dest('build/css'));
+  });
+   
+
+
+//   gulp.task('sass:watch', function () {
+//     gulp.watch('./sass/**/*.scss', ['sass']);
+//   });
+
+
+
+// ---------- SPRITE ----------
+
+gulp.task('sprite', function (cb) {
+    const spriteData = gulp.src('source/images/icons/*.png').pipe(spritesmith({
+      imgName: 'sprite.png',
+      imgPath: '../images/sprite.png',
+      cssName: 'sprite.scss'
+    }));
+    spriteData.img.pipe(gulp.dest('build/images/'));
+    spriteData.css.pipe(gulp.dest('source/styles/global/'));
+    cb();
+  });
+
+  // --------- Delete ---------
+
+  gulp.task('clean', function del(cb) {
+    return rimraf('build', cb);
+  });
+
+  //  ------ Copy fonts (from source to build)
+  gulp.task('copy:fonts', function(){
+    return gulp.src('./source/fonts/**/*.*')
+    .pipe(gulp.dest('build/fonts'));
+  });
+
+
+   //  ------ Copy Images (from source to build)
+   gulp.task('copy:images', function() {
+    return gulp.src('./source/images/**/*.*')
+    .pipe(gulp.dest('build/images'));
+  });
+
+  // ---------- Copy --------
+  gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'));
+
+  //  ------- WATCH -------
+  gulp.task('watch', function() {
+    gulp.watch('source/template/**/*.pug', gulp.series('templates:compile'));
+    gulp.watch('source/styles/**/*.scss', gulp.series('styles:compile'));
+  });
+
+  // ----- default----
+  gulp.task('default', gulp.series(
+  'clean',
+  gulp.parallel('templates:compile', 'styles:compile', 'sprite', 'copy'),
+  gulp.parallel('watch', 'server')
+  )
+  );
